47e94d41833ce0d9b9a3b80595aad81fcd47a0a0
ito-sann <n87.ito.yutaro@gmail.com>
2020-11-09 11:49:11 +0900

    動作確認1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :index]
-
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.all
@@ -20,13 +20,11 @@ class PrototypesController < ApplicationController
   end  
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
   
   def update
@@ -51,8 +49,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_tweet
+    @prototype = Prototype.find(params[:id])
+  end  
+
   def move_to_index
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless user_signed_in? && (current_user.id == @prototype.user_id)
       redirect_to action: :index
     end
   end
--- db/migrate/20201105015744_create_prototypes.rb
+++ db/migrate/20201105015744_create_prototypes.rb
@@ -3,7 +3,8 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
     create_table :prototypes do |t|
       t.string     :title,           null: false
       t.text       :catch_copy,      null: false
-      t.text       :concept,         null: false      
+      t.text       :concept,         null: false
+      t.text       :image    
       t.references :user,            foreign_key: true
       t.timestamps
     end
